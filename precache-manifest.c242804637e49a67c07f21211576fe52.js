f25aa3abffc6318e4123399cf5a178bb1057c6bb
Sreeraj Nair <sreeraj3545@gmail.com>
2019-09-21 00:34:35 +0400

    Updates

---
+++ precache-manifest.c242804637e49a67c07f21211576fe52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e89e740970a4a99774759bd9845ac71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9124b0e50e543c5ec62",
+    "url": "/static/css/main.42b9acda.chunk.css"
+  },
+  {
+    "revision": "547882bffd54bf0b0add",
+    "url": "/static/js/2.9c4c4d77.chunk.js"
+  },
+  {
+    "revision": "c9124b0e50e543c5ec62",
+    "url": "/static/js/main.d67b6095.chunk.js"
+  },
+  {
+    "revision": "a4e46a564f6329056d60",
+    "url": "/static/js/runtime~main.2b615656.js"
+  },
+  {
+    "revision": "0db4b70c7d32ebf79d80eb0aeedf6083",
+    "url": "/static/media/sreerajnair.0db4b70c.jpg"
+  }
+]);
