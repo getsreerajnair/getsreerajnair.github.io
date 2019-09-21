60e51ea3cd3d8e91a5058170df78604272e9e577
Sreeraj Nair <sreeraj3545@gmail.com>
2019-09-21 21:47:14 +0400

    Updates

---
+++ precache-manifest.d129e6c222c9348e4b353b8e13d5187c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae466f68697f36982ec3595eb4d0acca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ca6250c99458af18f2",
+    "url": "/static/css/main.012c175a.chunk.css"
+  },
+  {
+    "revision": "95cb8103c5dad05b64d5",
+    "url": "/static/js/2.ed448a8a.chunk.js"
+  },
+  {
+    "revision": "40ca6250c99458af18f2",
+    "url": "/static/js/main.8776f9ed.chunk.js"
+  },
+  {
+    "revision": "a4e46a564f6329056d60",
+    "url": "/static/js/runtime~main.2b615656.js"
+  },
+  {
+    "revision": "be6901d80f1783d0f01f6d9d4266e11d",
+    "url": "/static/media/DP.be6901d8.jpg"
+  },
+  {
+    "revision": "e34b08a93db6a46367c89e1b902c9f0f",
+    "url": "/static/media/mylogo.e34b08a9.jpg"
+  },
+  {
+    "revision": "0db4b70c7d32ebf79d80eb0aeedf6083",
+    "url": "/static/media/sreerajnair.0db4b70c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d6a9be069c1092768030184fde52c70.js"
+  "/precache-manifest.d129e6c222c9348e4b353b8e13d5187c.js"
 );
 
 self.addEventListener('message', (event) => {
